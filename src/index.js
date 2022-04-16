2bd90dd0298f5fe8a8d840c5401b92e8fae1f75d
Carlos Barreto <carlosmba96@gmail.com>
2022-04-16 17:56:05 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log("Hola brother");
+import style from "./style.css";
+
+function CreateHeader() {
+  const header = document.createElement("div");
+  header.classList.add("header");
+
+  const containerTitle = document.createElement("div");
+  containerTitle.classList.add("title-header");
+
+  const restaurantName = document.createElement("h1");
+  restaurantName.textContent = "Food In Code";
+
+  const navHeader = document.createElement("div");
+  navHeader.classList.add("nav-header");
+
+  const btnHome = document.createElement("button");
+  btnHome.classList.add("btn-home");
+  btnHome.textContent = "Home";
+
+  const btnMenu = document.createElement("button");
+  btnMenu.classList.add("btn-menu");
+  btnMenu.textContent = "Menu";
+
+  const btnContac = document.createElement("button");
+  btnContac.classList.add("btn-contact");
+  btnContac.textContent = "contact";
+
+  containerTitle.appendChild(restaurantName);
+  navHeader.appendChild(btnHome);
+  navHeader.appendChild(btnMenu);
+  navHeader.appendChild(btnContac);
+
+  header.appendChild(containerTitle);
+  header.appendChild(navHeader);
+
+  return header;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const rulesForJavaScript = {
   test: /\.js$/i,
@@ -7,14 +8,22 @@ const rulesForJavaScript = {
 };
 
 const rulesForStyles = {
-  test: /\.css$/,
-  use: ["style-loader", "css-loader"],
+  test: /\.css$/i,
+  use: [MiniCssExtractPlugin.loader, "css-loader"],
 };
 
-const rules = [rulesForJavaScript, rulesForStyles];
+const rulesForFiles = {
+  test: /\.(jpe?g|png|gif|svg|webp)$/i,
+  use: ["file-loader?name=assets/[name].[ext]"],
+};
+
+const rules = [rulesForJavaScript, rulesForStyles, rulesForFiles];
 
 module.exports = {
-  plugins: [new HtmlWebpackPlugin({ template: "src/index.html" })],
+  plugins: [
+    new HtmlWebpackPlugin({ template: "src/index.html" }),
+    new MiniCssExtractPlugin(),
+  ],
   module: { rules },
   devServer: {
     open: true,
