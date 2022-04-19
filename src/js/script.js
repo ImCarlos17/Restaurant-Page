64a54a61cf3bf13a0e64970472ca2d5e0862d9bb
Carlos Barreto <carlosmba96@gmail.com>
2022-04-19 10:57:38 -0400

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 import "../css/style.css";
+import indexModule from "../index.js";
+import homeContent from "../home.js";
+
+const renderIndex = indexModule;
+
+const titulo = document.createElement("h1");
+titulo.textContent = "Hola Webpack";
+
+const renderhome = homeContent();
+
+renderIndex.main.appendChild(renderhome);
