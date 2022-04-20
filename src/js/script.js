689e1ece8d2ebe69fb349b9a65e785a62eaa5e35
Carlos Barreto <carlosmba96@gmail.com>
2022-04-20 12:28:36 -0400

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,6 @@
 import "../css/style.css";
 import indexModule from "../index.js";
-import homeContent from "../home.js";
+import moduleMenu from "../menu";
 
 const renderIndex = indexModule;
-
-const titulo = document.createElement("h1");
-titulo.textContent = "Hola Webpack";
-
-const renderhome = homeContent();
-
-renderIndex.main.appendChild(renderhome);
+const renderMenu = moduleMenu();
